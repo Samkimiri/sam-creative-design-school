573d08090838487b27ef769d80069cfd5cf0fcd2
Samuel Kimiri <samcreativegraphics7@gmail.com>
2026-06-29 18:47:05 +0300

    Update TikTok social link

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -47,7 +47,7 @@ export default function Contact() {
   const socials = [
     { name: "Facebook", handle: "Sam Creatives", icon: "f", href: "https://www.facebook.com/profile.php?id=61591155443815" },
     { name: "Instagram", handle: "@samcreativegraphics", icon: "ig", href: "https://www.instagram.com/samcreativegraphics" },
-    { name: "TikTok", handle: "@samkim7", icon: "tk", href: "https://www.tiktok.com/@samkim7" },
+    { name: "TikTok", handle: "@samkim7", icon: "tk", href: "https://vm.tiktok.com/ZS96QHNaQA7Mk-cBfGz/" },
     { name: "LinkedIn", handle: "Samuel Ndung'u Kimiri", icon: "in", href: "https://www.linkedin.com/in/samuel-ndungu-kimiri" },
   ];
 
